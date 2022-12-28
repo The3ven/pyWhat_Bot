f97dcba309a8c8978878e2a5d87f29a04970c7c2
The3ven <the3ven@gmail.com>
2022-12-29 00:58:11 +0530

    Create main control statement method
    Signed-off-by: The3ven <the3ven@gmail.com>

--- Class_Shedule.py
+++ Class_Shedule.py
@@ -38,6 +38,33 @@ def get_sys_time_12_hr_sec():
     total_sec_12_hr = t.tm_sec + min_sec + hour_12_sec
     return total_sec_12_hr
 
+
+def get_time_in_sec(time: str = get_sys_time()):
+    if time.endswith("AM"):
+        time = time.replace("AM", "")
+    else:
+        time = time.replace("PM", "")
+    t = time.split(":")
+    h = t[0]
+    m = t[1]
+    s = t[2]
+    hour = int(h)
+    min = int(m)
+    sec = int(s)
+    sec_hour = hour*60*60
+    sec_min = min*60
+    total_sec = sec_hour + sec_min + sec
+    return total_sec  # ,hour,min,sec
+
+
+def get_sys_time_24_hr_sec():
+    t = time.localtime(time.time())
+    min_sec = t.tm_min*60
+    hr_24_sec = t.tm_hour*60*60
+    total_sec_24_hr = t.tm_sec + min_sec + hr_24_sec
+    return total_sec_24_hr
+
+
 def read_json(filename: str) -> dict:
     try:
         with open(filename, "r") as f:
@@ -96,19 +123,68 @@ def get_json_data(formattedJson: str, day: str, filename: str, Time: str, course
     return Sub, Sub_code, Prof, Description
 
 
+def main():
+    Times, timetable_json = get_class_time(MyJson, get_day_name())
+    total_sec_24_hr = get_sys_time_24_hr_sec()
+    last_flag = False
+    Sunday = (60*60*33 - 5)
+    Sleep_from_evening = (60*60*16 - 5)
+    count = 0
+    while True:
+        if get_day_name() == "Sunday":
+            time.sleep(Sunday)
+        if total_sec_12_hr == 18000:
+            time.sleep(Sleep_from_evening)
+        if total_sec_24_hr < 32390:
+            print("Sleeping For {}".format(32390 - total_sec_24_hr))
+            sleep_midnight = 32390 - total_sec_24_hr
+            time.sleep(sleep_midnight)
+        for Time in Times:
+            if Time == get_sys_time():
+                global last_time_run
+                last_time_run = Time
+                last_flag = True
+                print("Time For Class Sending Notification")
+                Sub, Sub_code, Prof, Description = get_json_data(
+                    timetable_json, get_day_name(), MyJson, Time, Course1)
+
+                if Sub is not None:
+                    message = ["You have Class Guys :-"]
+                    message = message + ["Subject : " + Sub]
+                else:
+                    message = []
+                if Sub_code is not None:
+                    message = message + ["Subject Code : " + Sub_code]
+                if Prof is not None:
+                    message = message + ["Professor : " + Prof]
+                if Description is not None:
+                    message = message + [Description]
+                send_messegse(MNumber, message)
+            else:
+                count = count + 1
+                print("Hello From Else L166")
+                if (count == 6):
+                    print(count)
+                    count = 0
+                    print("Count set to 0 again")
+                    if last_flag == False:
+                        print("Hello From Flag L171")
+                    else:
+                        print("line 176")
+                        sleep_sec = get_time_in_sec("00:01:00 ")
+                        print("Sleep for {} Second".format(sleep_sec))
+                        time.sleep(sleep_sec)
+
+
 if __name__ == "__main__":
     start = 5
     start_time = get_sys_time_12_hr_sec() + start
     while True:
         total_sec_12_hr = get_sys_time_12_hr_sec()
         remain = start_time - total_sec_12_hr
-        Times, timetable_json = get_class_time(MyJson, get_day_name())
-        print("Our Notification Bot Will Start in {}".format(remain))
-        for Time in Times:
-            if Time == get_sys_time():
-                print("Time For Class Sending Notification")
-                Sub, Sub_code, Prof, Description = get_json_data(
-                    timetable_json, get_day_name(), MyJson, Time, Course1)
-                message = ["You have Class Guys :-", "Subject : " + Sub,
-                        "Subject Code : " + Sub_code, "Professor : " + Prof, Description]
-                send_messegse(MNumber, message)
+        print(
+            "Our Notification Bot Will Start in {}".format(remain))
+        if total_sec_12_hr == start_time:
+            main()
+        else:
+            time.sleep(1)
